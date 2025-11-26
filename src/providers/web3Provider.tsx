956a2f54ed6110cfab66cc72c8193aa1bea51b4f
Abheek Tripathy <abheek.tripathy@gmail.com>
2025-11-26 23:54:26 +0530

    feat(wallet): change wallet provider to connectkit

--- src/providers/web3Provider.tsx
+++ src/providers/web3Provider.tsx
@@ -1,11 +1,8 @@
 "use client";
-import "@rainbow-me/rainbowkit/styles.css";
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-  type Chain,
-} from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
+
+import React from "react";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { WagmiProvider, createConfig, http } from "wagmi";
 import {
   mainnet,
   scroll,
@@ -16,6 +13,7 @@ import {
   avalanche,
   sophon,
   kaia,
+  type Chain,
 } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import config from "../../config";
@@ -37,13 +35,27 @@ const monad: Chain = {
     default: { name: "MonVision", url: config.chainBlockExplorerUrl },
   },
   testnet: config.chainTestnet,
-  iconUrl: config.chainIconUrl,
 };
 
-const wagmiConfig = getDefaultConfig({
-  appName: "Nexus Elements",
-  projectId: walletConnectProjectId,
+//ideally we should add private rpcs for each, for now it fallbacks to default rpcs
+const transports = {
+  [mainnet.id]: http(import.meta.env.VITE_MAINNET_RPC),
+  [base.id]: http(import.meta.env.VITE_BASE_RPC),
+  [arbitrum.id]: http(import.meta.env.VITE_ARBITRUM_RPC),
+  [optimism.id]: http(import.meta.env.VITE_OPTIMISM_RPC),
+  [polygon.id]: http(import.meta.env.VITE_POLYGON_RPC),
+  [scroll.id]: http(import.meta.env.VITE_SCROLL_RPC),
+  [avalanche.id]: http(import.meta.env.VITE_AVALANCHE_RPC),
+  [sophon.id]: http(import.meta.env.VITE_SOPHON_RPC),
+  [kaia.id]: http(import.meta.env.VITE_KAIA_RPC),
+  [monad.id]: http(config.chainRpcUrl),
+};
+
+const defaultConfigParams = getDefaultConfig({
+  appName: config.appTitle ?? "Nexus Elements",
+  walletConnectProjectId,
   chains: [
+    monad,
     mainnet,
     base,
     sophon,
@@ -53,19 +65,18 @@ const wagmiConfig = getDefaultConfig({
     optimism,
     polygon,
     scroll,
-    monad,
   ],
+  transports,
 });
 
+const wagmiConfig = createConfig(defaultConfigParams);
 const queryClient = new QueryClient();
 
 const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider initialChain={config.chainId} modalSize="compact">
-          {children}
-        </RainbowKitProvider>
+        <ConnectKitProvider theme="minimal">{children}</ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
