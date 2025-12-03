93d4c32c5d0cdc151f49579ac9de823627a4fb89
decocereus <amartyasinghkings07@gmail.com>
2025-12-04 01:25:04 +0530

    fix: react vuln

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  base: isProduction ? "/monad/" : "/",
   plugins: [
     react(),
     tailwindcss(),
