1d8898f055913c5bccb41137285d4db095efd7d1
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-01-12 16:22:58 +0530

    Update the config for html

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { writeFileSync } from "node:fs";
 import type { AppEnv } from "./src/vite-env.d";
-import { getConfig } from './config'
+import { getConfig } from "./getConfig";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_") as unknown as AppEnv;
@@ -68,12 +68,12 @@ export default defineConfig(({ mode }) => {
         transformIndexHtml(html) {
           // Replace placeholders with actual values
           return html
-            .replaceAll(/%= APP_TITLE %/g, config.meta.title)
-            .replaceAll(/%= APP_DESCRIPTION %/g, config.meta.description)
-            .replaceAll(/%= APP_CANONICAL_URL %/g, config.meta.canonicalUrl)
-            .replaceAll(/%= APP_FAVICON_URL %/g, config.meta.faviconUrl)
-            .replaceAll(/%= APP_THEME_COLOR %/g, config.meta.themeColor)
-            .replaceAll(/%= APP_META_IMAGE_URL %/g, config.meta.imageUrl)
+            .replaceAll(/%= APP_TITLE =%/g, config.meta.title)
+            .replaceAll(/%= APP_DESCRIPTION =%/g, config.meta.description)
+            .replaceAll(/%= APP_CANONICAL_URL =%/g, config.meta.canonicalUrl)
+            .replaceAll(/%= APP_FAVICON_URL =%/g, config.meta.faviconUrl)
+            .replaceAll(/%= APP_THEME_COLOR =%/g, config.meta.themeColor)
+            .replaceAll(/%= APP_META_IMAGE_URL =%/g, config.meta.imageUrl)
         }
       }
     ],
