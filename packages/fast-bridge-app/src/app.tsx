c92655abdb698e24f267fad9eb7258eccb219c7d
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-03-13 22:36:09 +0530

    deploy debugnet for protocol fee

--- packages/fast-bridge-app/src/app.tsx
+++ packages/fast-bridge-app/src/app.tsx
@@ -1,3 +1,4 @@
+import { Environment } from "@avail-project/nexus-core";
 import { appConfig, chainFeatures } from "@fastbridge/runtime";
 import { motion } from "motion/react";
 import FastBridgeShowcase from "@/components/fast-bridge-showcase";
@@ -12,18 +13,18 @@ export default function App() {
     <Web3Provider>
       <NexusProvider
         config={{
-          // network: {
-          //   NETWORK_HINT: Environment.CERISE,
-          //   COSMOS_GRPC_URL: "https://debugnet.availproject.org/grpc-web/",
-          //   COSMOS_REST_URL: "https://debugnet.availproject.org",
-          //   COSMOS_RPC_URL: "https://debugnet.availproject.org:26650",
-          //   COSMOS_WS_URL: "wss://debugnet.availproject.org:26650/websocket",
-          //   INTENT_EXPLORER_URL:
-          //     "https://explorer.nexus-cerise.availproject.org",
-          //   VSC_BASE_URL: "https://vsc-debugnet.availproject.org",
-          //   VSC_WS_URL: "wss://vsc-debugnet.availproject.org",
-          // },
-          network: "mainnet",
+          network: {
+            NETWORK_HINT: Environment.CERISE,
+            COSMOS_GRPC_URL: "https://debugnet.availproject.org/grpc-web/",
+            COSMOS_REST_URL: "https://debugnet.availproject.org",
+            COSMOS_RPC_URL: "https://debugnet.availproject.org:26650",
+            COSMOS_WS_URL: "wss://debugnet.availproject.org:26650/websocket",
+            INTENT_EXPLORER_URL:
+              "https://explorer.nexus-cerise.availproject.org",
+            VSC_BASE_URL: "https://vsc-debugnet.availproject.org",
+            VSC_WS_URL: "wss://vsc-debugnet.availproject.org",
+          },
+          // network: "mainnet",
           debug: true,
         }}
       >
