096a39e76b5787a5b150b1e01cff4cf532f2c17c
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-09-15 16:47:52 +0530

    Update the sdk and use canary

--- packages/fast-bridge-app/src/components/nexus/nexus-provider.tsx
+++ packages/fast-bridge-app/src/components/nexus/nexus-provider.tsx
@@ -230,6 +230,7 @@ const NexusProvider = ({
     setNexusInitError(null);
     console.log("NEXUS CONFIG", stableConfig);
     const nextSdk = createNexusClient({
+      clientId: "nexus-fast-bridge",
       network: stableConfig.network,
       debug: stableConfig.debug,
     });
@@ -574,6 +575,7 @@ const NexusProvider = ({
       try {
         console.log("INITIALIZE NEXUS CONFIG", stableConfig);
         const nextSdk = createNexusClient({
+          clientId: "nexus-fast-bridge",
           network: stableConfig.network,
           debug: stableConfig.debug,
         });
