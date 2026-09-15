036646bd5d5b3e44cb1ef81e705340623ce6a4a8
shrinathprabhu <shrinathprabhu.123@gmail.com>
2026-09-15 16:59:04 +0530

    remove getbalancesforbridge

--- packages/fast-bridge-app/src/components/nexus/nexus-provider.tsx
+++ packages/fast-bridge-app/src/components/nexus/nexus-provider.tsx
@@ -515,7 +515,7 @@ const NexusProvider = ({
       const [bridgeAbleBalanceResult, swapBalanceResult, rates] =
         await withTimeout(
           Promise.allSettled([
-            activeSdk.getBalancesForBridge(),
+            Promise.resolve([]),
             activeSdk.getBalancesForSwap(),
             getCoinbaseRates(),
           ]),
@@ -653,10 +653,7 @@ const NexusProvider = ({
       if (!activeSdk) {
         return;
       }
-      const updatedBalance = await withTimeout(
-        activeSdk.getBalancesForBridge(),
-        15_000
-      );
+      const updatedBalance = await withTimeout(Promise.resolve([]), 15_000);
       const chains = supportedChainsAndTokens.current ?? [];
       setBridgableBalance(
         normalizeUserAssetFiatValues(
